b260c0a9e8845bf0c6747467d4acf8cb34128409
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2023-03-04 23:43:33 -0500

    publicacion
    4-32023

--- setup.py
+++ setup.py
@@ -2,12 +2,26 @@ from setuptools import setup
 setup(
 
 	name="RenePython",
-	version="1.0",
+	version="0.9",
 	description="genial",
-	author="Rene",
+	author="René Lázaro Collado Arteaga",
 	author_email="renearteaga261998@gmail.com",
-	packages=["RenePy","RenePy.MetodosUtiles","RenePy.ClasesUtiles","RenePy.ClasesUtiles.Tipos"
-		,"RenePy.ClasesUtiles.BasesDeDatos","RenePy.ClasesUtiles.Interfaces","RenePy.MetodosUtiles.Imports"],
-	url="www.google.com"
+	license="GPLv3",
+	include_package_data=True,
+	packages=["RenePy"
+
+		,"RenePy.ClasesUtiles"
+		,"RenePy.ClasesUtiles.BasesDeDatos"
+		,"RenePy.ClasesUtiles.BasesDeDatos.Factory"
+		,"RenePy.ClasesUtiles.Interfaces"
+		,"RenePy.ClasesUtiles.Tipos"
+		,"RenePy.ClasesUtiles.Interfaces"
+		,"RenePy.MetodosUtiles"
+		,"RenePy.MetodosUtiles.Imports"],
+	url="https://github.com/critBus/RenePython",
+	classifiers = ["Programming Language :: Python :: 3",
+	"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+	"Development Status :: 4 - Beta", "Intended Audience :: Developers",
+	"Operating System :: OS Independent"],
 	
 	)
